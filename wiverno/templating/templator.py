884b93eb68e290cbd25c70a76cf2099b8652c898
Sayrrexe <sayrrexe@gmail.com>
2025-06-22 15:49:08 +0300

    refactor(templator): update base directory handling and improve template loading

--- wiverno/templating/templator.py
+++ wiverno/templating/templator.py
@@ -1,14 +1,17 @@
+import os
+
 from pathlib import Path
+from typing import Optional
 from jinja2 import Environment, FileSystemLoader
 
 class Templator:
     def __init__(self, folder: str = "templates"):
 
         self.env = Environment()
-        # Base directory of the framework package
-        self.base_dir = Path(__file__).resolve().parent
-        # Load templates from the provided folder relative to the package
-        self.env.loader = FileSystemLoader(self.base_dir / folder)
+        # Define the base directory of the project
+        self.base_dir = os.getcwd()
+        # Set the loader to the templates folder within the my_app directory
+        self.env.loader = FileSystemLoader(os.path.join(self.base_dir, folder))
         
     def render(self, template_name: str, content: Optional[dict] = None, **kwargs):
         """
@@ -25,4 +28,4 @@ class Templator:
         if not isinstance(content, dict):
             raise TypeError("Content must be a dictionary.")
         # Render the template with the provided context
-        return template.render(content, **kwargs)
+        return template.render(**content, **kwargs)
